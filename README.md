0643e6baecc9522a212e1f0c2828ead898f0be07
Romello Goodman <romellogood@gmail.com>
2021-03-14 10:32:43 -0400

    first pass at the library

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 A set of components for working with SVGs in React.
 
+## Usage
+
 ### Install
 
 ```
@@ -18,7 +20,11 @@ import { SVG, Circle } from "react-goodgraphics";
 
 const FooBar = () => {
   return (
-    <SVG height={400} width={400}>
+    <SVG
+      height={400}
+      width={400}
+      style={{ background: "#1b1b1b", fill: "white" }}
+    >
       <Circle x={200} y={200} radius={100} />
     </SVG>
   );
@@ -26,3 +32,43 @@ const FooBar = () => {
 
 export default FooBar;
 ```
+
+## API
+
+### SVG
+
+- `@prop {Number} height` Height of the svg. Defaults to 200.
+- `@prop {Number} width` Width of the svg. Defaults to 200.
+
+### `Circle`
+
+- `@prop {String | Number} x` The x position for the shape.
+- `@prop {String | Number} y` The y position for the shape.
+- `@prop {String | Number} radius` The radius of the shape.
+
+### `Ellipse`
+
+- `@prop {String | Number} x` The x position for the shape.
+- `@prop {String | Number} y` The y position for the shape.
+- `@prop {String | Number} width` The width of the shape.
+- `@prop {String | Number} height` The height of the shape.
+
+### `Rect`
+
+- `@prop {String | Number} x` The x position for the shape.
+- `@prop {String | Number} y` The y position for the shape.
+- `@prop {String | Number} width` The width of the shape.
+- `@prop {String | Number} height` The height of the shape.
+
+### `Square`
+
+- `@prop {String | Number} x` The x position for the shape.
+- `@prop {String | Number} y` The y position for the shape.
+- `@prop {String | Number} size` The size of the square.
+
+### `Line`
+
+- `@prop {String | Number} x1` The x position of the start of the line.
+- `@prop {String | Number} y1` The y position of the start of the line.
+- `@prop {String | Number} x2` The x position of the end of the line.
+- `@prop {String | Number} y2` The y position of the end of the line.
--- react-goodgraphics.js
+++ react-goodgraphics.js
@@ -1,1 +1,90 @@
 import React from "react";
+
+/**
+ ********************
+ * Utils
+ ********************
+ */
+
+/**
+ * Map a number from one range to another range
+ * https://gist.github.com/xposedbones/75ebaef3c10060a3ee3b246166caab56
+ */
+const map = (number, inMin, inMax, outMin, outMax) => {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
+
+/**
+ ********************
+ * Components
+ ********************
+ */
+
+export const SVG = (props) => {
+  const { children, height, width, ...restProps } = props;
+
+  return (
+    <svg height={height} width={width} {...restProps}>
+      {children}
+    </svg>
+  );
+};
+
+export const Circle = (props) => {
+  const { x, y, radius, ...restProps } = props;
+
+  return <circle cx={x} cy={y} r={radius} {...restProps} />;
+};
+
+export const Ellipse = (props) => {
+  const { x, y, height, width, ...restProps } = props;
+
+  return <ellipse cx={x} cy={y} rx={height} ry={width} {...restProps} />;
+};
+
+export const Rect = (props) => {
+  const { x, y, height, width, ...restProps } = props;
+
+  return <rect x={x} y={y} height={height} width={width} {...restProps} />;
+};
+
+export const Square = (props) => {
+  const { x, y, size, ...restProps } = props;
+
+  return <rect x={x} y={y} height={size} width={size} {...restProps} />;
+};
+
+export const Line = (props) => {
+  const { x1, y1, x2, y2, ...restProps } = props;
+
+  return <line x1={x1} y1={y1} x2={x2} y2={y2} {...restProps} />;
+};
+
+export const Times = (props) => {
+  const { draw, number } = props;
+
+  return new Array(number).fill(null).map((_, index) => draw({ index }));
+};
+
+export const Grid = (props) => {
+  const { columns, rows, draw, height, width, margin } = props;
+
+  return new Array(columns).fill(null).map((_, colIndex) => {
+    return new Array(rows).fill(null).map((_, rowIndex) => {
+      const halfMargin = margin / 2;
+      const posX = map(colIndex, 0, columns, halfMargin, width - halfMargin);
+      const posY = map(rowIndex, 0, rows, halfMargin, height - halfMargin);
+      const cellHeight = (height - margin) / rows;
+      const cellWidth = (width - margin) / columns;
+
+      return draw({
+        colIndex,
+        rowIndex,
+        posX,
+        posY,
+        cellHeight,
+        cellWidth,
+      });
+    });
+  });
+};
